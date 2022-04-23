e668b38ababcbe472b7cf5b9d72d8b1afdc63f36
Brenner Spear <blspear@gmail.com>
2022-04-23 14:11:23 -0500

    evm address support

--- src/__tests__/web3-names.test.ts
+++ src/__tests__/web3-names.test.ts
@@ -1,5 +1,62 @@
-import addressToName from '../index'
+import { addressToName, addressToNameObject } from '../index'
+
+const addressUppercaseWith0x = '0x17A059B6B0C8af433032d554B0392995155452E6'
+const addressUppercaseWithout0x = '17A059B6B0C8af433032d554B0392995155452E6'
+const addresslowercaseWith0x = '0x17a059b6b0c8af433032d554b0392995155452e6'
+const addresslowercaseWithout0x = '17a059b6b0c8af433032d554b0392995155452e6'
+
+const brennerNameObject = {
+    name: 'Felicita Feeney',
+    prefix: 'Miss',
+    firstName: 'Felicita',
+    middleName: 'Micah',
+    lastName: 'Feeney',
+}
+
+const miguelNameObject = {
+    name: 'Augusta Schuppe',
+    prefix: 'Ms.',
+    firstName: 'Augusta',
+    middleName: 'Shawn',
+    lastName: 'Schuppe',
+}
+
+const miguelAddress = '0xe340b00b6b622c136ffa5cff130ec8edcddcb39d'
+
+const big1 = '0xfffffffffffff000000000000000000000000000'
+const big2 = '0xffffffffffffe000000000000000000000000000'
+const small1 = '0x0000000000000000000000000000000000000000'
+const small2 = '0x0000000000001000000000000000000000000000'
 
 test('addressToName', () => {
-    expect(addressToName('address')).toBe('addressa name')
+    const name1 = addressToName(addressUppercaseWith0x)
+    const name2 = addressToName(addressUppercaseWithout0x)
+    const name3 = addressToName(addresslowercaseWith0x)
+    const name4 = addressToName(addresslowercaseWithout0x)
+
+    expect(name1).toEqual(brennerNameObject.name)
+    expect(name1).toEqual(name2)
+    expect(name1).toEqual(name3)
+    expect(name1).toEqual(name4)
+
+    const miguelName = addressToName(miguelAddress)
+    expect(miguelName).toEqual(miguelNameObject.name)
+
+    expect(addressToName(big1)).not.toEqual(addressToName(big2))
+    expect(addressToName(small1)).not.toEqual(addressToName(small2))
+
+    // solana address, don't know how to parse these into a number, someone please add this
+    expect(() => addressToName('66WJDgCrc3SwUXdstY6u5wAnxRyWRT8veawJNfn6QxKo')).toThrow()
+})
+
+test('addressToNameObject', () => {
+    const name1 = addressToNameObject(addressUppercaseWith0x)
+    const name2 = addressToNameObject(addressUppercaseWithout0x)
+    const name3 = addressToNameObject(addresslowercaseWith0x)
+    const name4 = addressToNameObject(addresslowercaseWithout0x)
+
+    expect(name1).toEqual(brennerNameObject)
+
+    expect(addressToNameObject(big1)).not.toEqual(addressToNameObject(big2))
+    expect(addressToNameObject(small1)).not.toEqual(addressToNameObject(small2))
 })
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,62 @@
-function addressToName(address: string): string {
-    return address + 'a name'
+import { Faker } from '@faker-js/faker'
+import faker from '@faker-js/faker/locale/en'
+
+const validAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/)
+
+export const validateAddress = (address: string): string => {
+    const lowercaseAddress = address.toLowerCase()
+    const with0xAddress = lowercaseAddress.slice(0, 2) === '0x' ? address : `0x${address}`
+    if (!validAddress.test(with0xAddress)) {
+        throw new Error(`Invalid address: ${address}. Only evm addresses are supported.`)
+    }
+    return with0xAddress
 }
 
-export default addressToName
+function getFaker(address: string): Faker {
+    const normalizedAddress = validateAddress(address)
+
+    // faker needs a number, and javascript loses precision at 15 hexedecimal digits
+    const sixteenOnly = normalizedAddress.slice(0, 15)
+
+    const number = parseInt(sixteenOnly, 16)
+
+    faker.seed(number)
+    return faker
+}
+
+export type NameObject = {
+    name: string
+    prefix: string
+    firstName: string
+    middleName: string
+    lastName: string
+}
+
+function getNameObject(faker: Faker): NameObject {
+    // every time you call a function on faker, it shakes the dice. If we want to add more things about the name, they must get added below the last call to faker
+    const firstName = faker.name.firstName()
+    const middleName = faker.name.middleName()
+    const lastName = faker.name.lastName()
+    const prefix = faker.name.prefix()
+
+    return {
+        name: firstName + ' ' + lastName,
+        prefix,
+        firstName,
+        middleName,
+        lastName,
+    }
+}
+
+function addressToName(address: string): string {
+    const faker = getFaker(address)
+    const nameObject = getNameObject(faker)
+    return nameObject.name
+}
+
+function addressToNameObject(address: string): NameObject {
+    const faker = getFaker(address)
+    return getNameObject(faker)
+}
+
+export { addressToName, addressToNameObject }
